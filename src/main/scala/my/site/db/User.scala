6ca701159d76e60776bcfddf595d2771062a3acc
Darth Vader_2k17 <7552494@gmail.com>
2023-03-16 13:05:33 +0300

    <3

--- src/main/scala/my/site/db/User.scala
+++ src/main/scala/my/site/db/User.scala
@@ -5,19 +5,19 @@ import scalikejdbc._
 
 object User {
   def reqUser(name: String): Option[User] = {
-    implicit val session = AutoSession
+    implicit val session: AutoSession.type = AutoSession
     sql"""SELECT * FROM Users WHERE name = $name"""
       .map(rs => User(rs.int("user_id"), rs.string("login"), rs.string("password"),
         rs.string("email"), rs.boolean("mailing"), rs.string("birthday"))).single.apply()
   }
 
   def getUserId(name: String): Option[Int] = {
-    implicit val session = AutoSession
+    implicit val session: AutoSession.type = AutoSession
     sql"""SELECT * FROM Users WHERE name = $name""".map(rs => rs.int("user_id")).single.apply()
   }
 
   def allUsers(): Seq[User] = {
-    implicit val session = AutoSession
+    implicit val session: AutoSession.type = AutoSession
     sql"""SELECT * FROM Users""".map(rs => User(rs.int("user_id"), rs.string("login"), rs.string("password"),
       rs.string("email"), rs.boolean("mailing"), rs.string("birthday"))).list.apply()
   }
